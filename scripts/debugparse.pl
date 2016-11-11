618443e94a1fd828ba1d603e4baeac4fffecd25a
Xiaotong Sun <sancruise123@gmail.com>
2016-11-10 22:29:26 -0500

    remove network simulation files

--- scripts/debugparse.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-# Parse a debug.h file to create a debug-text.h file.
-
-while (<STDIN>) {
-  if (/^#define ([^\s]+)\s.*DBTEXT:\s+(.*)/) {
-                print "{ $1, \"$2\"},\n";
-        }
-}
